3fe441c098ac90c13ee05bc4db99767561fdfb90
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2023-12-14 14:30:11 +0100

    added a update student by id endpoint

--- src/main/java/com/demo/student/StudentService.java
+++ src/main/java/com/demo/student/StudentService.java
@@ -1,9 +1,11 @@
 package com.demo.student;
 
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
 
 @Service
@@ -34,5 +36,25 @@ public class StudentService {
             throw new IllegalStateException("Student with the id "+studentId+ " does not exist");
         }
         studentRepository.deleteById(studentId);
+    }
+
+    @Transactional
+    public void updateStudent(Long studentId, String name, String email) {
+       Student student = studentRepository.findById(studentId)
+               .orElseThrow(() -> new IllegalStateException(
+                       "Student with id "+ studentId + " does not exist"
+               ));
+        if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+            student.setName(name);
+        }
+
+        if (email != null && name.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+            Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+            if (studentOptional.isPresent()) {
+                throw new IllegalStateException("email taken");
+            }
+            student.setEmail(email);
+        }
+
     }
 }
