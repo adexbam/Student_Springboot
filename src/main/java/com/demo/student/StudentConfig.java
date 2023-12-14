2bec85a3ac8783a1b6da716c1b709b7a4a77f56d
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2023-12-14 12:41:07 +0100

    configured student age to be Transient

--- src/main/java/com/demo/student/StudentConfig.java
+++ src/main/java/com/demo/student/StudentConfig.java
@@ -16,22 +16,19 @@ public class StudentConfig {
             Student mariam = new Student(
                     "mariam",
                     "mm@mdmdm.tr",
-                    LocalDate.of(2000, Month.APRIL, 4),
-                    21
+                    LocalDate.of(2000, Month.APRIL, 4)
             );
 
             Student mark = new Student(
                     "mark",
                     "mark@mdmdm.tr",
-                    LocalDate.of(2001, Month.JUNE, 4),
-                    20
+                    LocalDate.of(2001, Month.JUNE, 4)
             );
 
             Student alex = new Student(
                     "alex",
                     "alex@mdmdm.tr",
-                    LocalDate.of(2002, Month.JANUARY, 4),
-                    19
+                    LocalDate.of(2002, Month.JANUARY, 4)
             );
 
             repository.saveAll(List.of(mariam, mark, alex));
