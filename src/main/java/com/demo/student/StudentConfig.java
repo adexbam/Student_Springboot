74b18610e3713dca3223f1939af87fe278de3455
adewunmi bamishigbin <32496428+adexbam@users.noreply.github.com>
2023-12-14 12:21:45 +0100

    configured a command to add some students

---
+++ src/main/java/com/demo/student/StudentConfig.java
@@ -0,0 +1,41 @@
+package com.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student mariam = new Student(
+                    "mariam",
+                    "mm@mdmdm.tr",
+                    LocalDate.of(2000, Month.APRIL, 4),
+                    21
+            );
+
+            Student mark = new Student(
+                    "mark",
+                    "mark@mdmdm.tr",
+                    LocalDate.of(2001, Month.JUNE, 4),
+                    20
+            );
+
+            Student alex = new Student(
+                    "alex",
+                    "alex@mdmdm.tr",
+                    LocalDate.of(2002, Month.JANUARY, 4),
+                    19
+            );
+
+            repository.saveAll(List.of(mariam, mark, alex));
+
+        };
+    }
+}
